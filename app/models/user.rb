92b71374e32f58d063a42e2c94617c4263ea97bc
Office-asami <s-wakita@office-asami.com>
2022-07-16 17:01:55 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
-  validates :family_name_kana, presence: true, format: { with: /\p{katakana}/}
-  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/}
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
   validates :birth_day, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
 
 end
