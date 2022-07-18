46ba9b616292f29102c40f6775fe5ea4f64f7d59
Office-asami <s-wakita@office-asami.com>
2022-07-18 19:41:21 +0900

    データベースUTF-8コードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :family_name
-    validates :first_name
-  end
-  
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
-    validates :family_name_kana
-    validates :first_name_kana
-  end
-
-
 end
