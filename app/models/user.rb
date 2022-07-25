e7c85f409a6a02a7aebecaf8c72d14658c694c0e
Office-asami <s-wakita@office-asami.com>
2022-07-25 22:12:09 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   validates :birth_day, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  validates :family_name, format: { with: VALID_NAME_REGEX }
+  validates :first_name, format: { with: VALID_NAME_REGEX }
+  VALID_NAMEKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :family_name, format: { with: VALID_NAMEKANA_REGEX }
+  validates :first_name, format: { with: VALID_NAMEKANA_REGEX }
+
 end
