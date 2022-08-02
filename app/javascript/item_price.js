a0eafab6bc8c9691fd07d153e17bfb4c5f11ee52
Office-asami <s-wakita@office-asami.com>
2022-08-02 20:16:03 +0900

    デバッグツールの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,9 @@
 function item_price (){
 
-  console.log("商品価格を計算");
-
   const rate = 0.1;
   const item_price = document.getElementById("item-price");
 
   item_price.addEventListener("keyup", () => {
-    console.log(item_price.value)
     const sales_commission = Math.floor(item_price.value * rate);
     sales_commission_St = sales_commission.toLocaleString();
     const sales_profit = item_price.value - sales_commission
