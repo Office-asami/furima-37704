f1d94e315a0a11f48c2d9a71009d6e61f1cbe4ad
Office-asami <s-wakita@office-asami.com>
2022-08-11 10:34:15 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_many :items
-  # has_many :buyers
+  has_many :buyers
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
