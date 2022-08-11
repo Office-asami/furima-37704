23de8b9182f7e9cc3313acca7d05994b520cdf43
Office-asami <s-wakita@office-asami.com>
2022-08-11 10:29:04 +0900

    Buyerモデル、Shippingモデルの作成

--- app/models/buyer_shipping.rb
+++ app/models/buyer_shipping.rb
@@ -1,17 +1,6 @@
 class BuyerShipping
   include ActiveModel::Model
-  atter_accessor :user_id, :item, :zip_code, :prefecture_id, :city, :address1, :address2, :telephone
-
-  with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
-    validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-  
-  end
-
-
+  atter_accessor :user_id, :item, :zip_code, :prefecture, :city, :address1, :address2, :telephone, :buyer_id
 
   def save
   end
