64cbf9ecfc3c9d6826a4298ec61881a238cc642f
Office-asami <s-wakita@office-asami.com>
2022-08-13 09:57:23 +0900

    記述の簡略化

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,7 +1,9 @@
 class BuyersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :find_item, only:[:index, :create]
 
   def index
-    @item = Item.find(params[:item_id]) 
+    # @item = Item.find(params[:item_id]) 
     @buyer_shipping = BuyerShipping.new
   end
 
@@ -11,7 +13,7 @@ class BuyersController < ApplicationController
   def create
     @buyer_shipping = BuyerShipping.new(buyer_params)
     if @buyer_shipping.valid?
-      @item = Item.find(params[:item_id]) 
+      # @item = Item.find(params[:item_id]) 
       Payjp.api_key = "sk_test_292b681c66743e76f39e6c20"
       Payjp::Charge.create(
         amount: @item[:price],
@@ -21,7 +23,7 @@ class BuyersController < ApplicationController
       @buyer_shipping.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id]) 
+      # @item = Item.find(params[:item_id]) 
       render :index
     end
   end
@@ -31,6 +33,10 @@ class BuyersController < ApplicationController
 
   def buyer_params
     params.require(:buyer_shipping).permit(:zip_code, :prefecture_id, :city, :address1, :address2, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id]) 
   end
 
 end
