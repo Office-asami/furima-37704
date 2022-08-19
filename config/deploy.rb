b0f7adfee0eac04925bec3af1799c8c8c0ee4565
Office-asami <s-wakita@office-asami.com>
2022-08-19 12:13:03 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ lock '3.17.1'
 set :application, 'furima-37704'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'Office-asami/furima-37704.git'
+set :repo_url,  'git@github.com:Office-asami/furima-37704.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
